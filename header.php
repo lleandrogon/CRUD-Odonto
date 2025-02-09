896fd0ee64652dd1a38b8a3853e6f3eeb63f1963
Leandro Gonçalves <159172886+lleandrogon@users.noreply.github.com>
2025-02-09 14:20:13 -0300

    Primeiro commit

---
+++ header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/estilo.css">
+    <title>Togni Odontologia</title>
+</head>
+<body>
+    <header class="cabecalho container-fluid">
+        <img src="https://www.clinicatogniodontologia.com/wp-content/uploads/2023/11/cropped-logotipo.png" alt="Logo" class="logo-cabecalho img-fluid">
+    </header>
+</body>
+</html>
---
+++ views/pacientes.php
@@ -0,0 +1,43 @@
+<div class="pacientes d-flex justify-content-center align-items-center">
+    <div class="tabela-container"> 
+        <table class="table table-striped text-center">
+            <h2 class="titulo">Gerenciamento de Pacientes</h2>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Nome</th>
+                    <th>CPF</th>
+                    <th>Telefone</th>
+                    <th>Editar</th>
+                    <th>Deletar</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>Leandro Gonçalves Crisóstomo</td>
+                    <td>999.999.999-99</td>
+                    <td>(99) 99999-9999</td>
+                    <td><button class="botao"><i class="fa-solid fa-pen"></i></button></td>
+                    <td><button class="botao"><i class="fa-solid fa-trash"></i></button></td>
+                </tr>
+                <tr>
+                    <td>2</td>
+                    <td>Leandro Gonçalves Crisóstomo</td>
+                    <td>999.999.999-99</td>
+                    <td>(99) 99999-9999</td>
+                    <td><button class="botao"><i class="fa-solid fa-pen"></i></button></td>
+                    <td><button class="botao"><i class="fa-solid fa-trash"></i></button></td>
+                </tr>
+                <tr>
+                    <td>3</td>
+                    <td>Leandro Gonçalves Crisóstomo</td>
+                    <td>999.999.999-99</td>
+                    <td>(99) 99999-9999</td>
+                    <td><button class="botao"><i class="fa-solid fa-pen"></i></button></td>
+                    <td><button class="botao"><i class="fa-solid fa-trash"></i></button></td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
